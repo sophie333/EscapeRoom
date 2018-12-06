11e2bba1ec0f2d72d4eec645218efad16cfd67b6
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-12-06 22:51:26 +0100

    fixed boxinteraction, added drawer animation

--- EscapeRoom/Assets/Scripts/BoxInteraction.cs
+++ EscapeRoom/Assets/Scripts/BoxInteraction.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
+using System.Collections;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class BoxInteraction : MonoBehaviour
 {
-    [SerializeField] private GameObject text;
+    [SerializeField] private Animator anim;
+    [SerializeField] private GameObject interactText;
+    [SerializeField] private GameObject codeText;
+    [SerializeField] private GameObject heartKey;
+    [SerializeField] private string actualCode = "625";
+    private KeyCode[] keycodeArray = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9 };
     private Transform m_transform;
     private Vector3 playerPos;
     private bool isInteracting;
+    private bool solved;
+    private string code = "";
+    private int counter = 0;
 
     private void Start()
     {
@@ -19,37 +28,89 @@ public class BoxInteraction : MonoBehaviour
 
         if (player)
         {
-            text.SetActive(true);
-            
-            if (!isInteracting)
+            if (!isInteracting && !solved)
             {
+                interactText.SetActive(true);
                 playerPos = player.transform.position;
+
                 if (Input.GetKeyDown(KeyCode.R))
                 {
                     isInteracting = true;
-                    text.SetActive(false);
+                    interactText.SetActive(false);
                     player.GetComponent<FirstPersonController>().enabled = false;
-                    player.transform.position = new Vector3(0.113f, 0.537f, -1.76f);
+                    player.transform.position = new Vector3(0.08f, 0.537f, -1.76f);
                     player.transform.rotation = Quaternion.Euler(0, 0, 0);
                     player.transform.GetChild(0).gameObject.transform.rotation = Quaternion.Euler(90, -90, 0);
                 }
             }
             else
             {
-                if (Input.GetKeyDown(KeyCode.R))
+                EnterCode();
+                if (Input.GetKeyDown(KeyCode.F))
                 {
-                    isInteracting = false;
-                    text.SetActive(true);
-                    player.transform.position = playerPos;
+                    isInteracting = false;                    
+                    player.transform.position = new Vector3(1.35f, 1.02f, -1.8f); //playerPos;
                     player.transform.eulerAngles = new Vector3(0, 0, 0);
                     player.GetComponent<FirstPersonController>().enabled = true;
+                    if (!solved)
+                    {
+                        interactText.SetActive(true);
+                    }
+                    else
+                    {
+                        anim.Play("OpenDrawer");
+                        StartCoroutine(WaitAndReveal(1.0f));
+                    }
                 }
             }
         }
     }
 
+    IEnumerator WaitAndReveal(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        heartKey.GetComponent<Rigidbody>().isKinematic = false;
+        Debug.Log("no more kinematic");
+    }
+
     private void OnTriggerExit(Collider other)
     {
-        text.SetActive(false);
+        interactText.SetActive(false);
+    }
+
+    private void EnterCode()
+    {
+        string key = "";
+        if (!solved)
+        {
+            foreach (KeyCode vKey in keycodeArray)
+            {
+                if (Input.GetKeyDown(vKey))
+                {
+                    key = vKey.ToString();
+                    counter++;
+                }
+            }
+            if (key != "")
+            {
+                code += key[5];
+                codeText.GetComponent<TextMesh>().text = code;
+            }
+        }
+
+        if (counter >= 3)
+        {
+            if (code == actualCode)
+            {
+                Debug.Log("hurrary");
+                solved = true;
+            }
+            else
+            {
+                code = "";
+                codeText.GetComponent<TextMesh>().text = "___";
+                counter = 0;
+            }
+        }
     }
 }
